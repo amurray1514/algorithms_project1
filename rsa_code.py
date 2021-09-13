b1f51b1c87723fdb902c96ff647cd558e0099e96
Archer Murray <amurray@cub.uca.edu>
2021-09-13 12:52:54 -0500

    Added integration test

--- rsa_code.py
+++ rsa_code.py
@@ -1,8 +1,13 @@
-import math
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This is the code implementing RSA.
+
+@author: Josh Hicks
+"""
+
 from math import gcd
 import random
-
-message = "qwertyuiopasdfghjklzxcvbnm"
 
 def encrypt(mess, _e, _n):
     m2 = []
@@ -23,17 +28,15 @@ def relativePrime(num):
     rels = []
     for i in range(2,num):
         if gcd(num, i) == 1:
-            rels.append( i)
-    return rels[random.randint(0,len(rels) - 1)]
+            rels.append(i)
+    return rels[random.randint(0, len(rels) - 1)]
 
 def inverse(_e, _phi):
-    for i in range(2,phi):
+    for i in range(2, _phi):
         if (_e * i) % _phi == 1:
             return i
 
-        
-    
-def getPrimeNumbers(m = 1000 ,n = 1000):      
+def getPrimeNumbers(m = 1000, n = 1000):      
     # Create a boolean array "prime[0..n]" and
     # initialize all entries it as true. A value
     # in prime[i] will finally be false if i is
@@ -42,16 +45,15 @@ def getPrimeNumbers(m = 1000 ,n = 1000):
     p = 2
     while(p * p <= n):          
         # If prime[p] is not changed, then it is
-       # a prime
+        # a prime
         if (prime[p] == True):
-              
             # Update all multiples of p
             for i in range(p * p, n + 1, p):
                 prime[i] = False
         p += 1
     #print(len(prime))
     primes = []
-    # Print all prime numbers
+    # Return all prime numbers
     for p in range(m, n):
         if prime[p]:
             primes.append(p)
@@ -62,23 +64,16 @@ primeNumbers = getPrimeNumbers(150, 1000)
 def getRandomPrime():
     return primeNumbers[random.randint(0, len(primeNumbers))]
 
-#print(primeNumbers)    
-#Random prime numbers
-p = getRandomPrime()
-q = getRandomPrime()
-
-#Public Key
-n = p * q
-phi = (p - 1 ) * ( q - 1)
-e = relativePrime(phi)
-
-#Private Key
-d = inverse(e,phi)
-#print(d)
-
-#print(relativePrime(phi))    
-encrypted = encrypt(message, e, n)
-#print(encrypted)
-
-decrypted = decrypt(encrypted, d, n)
-print(decrypted)
+def getRSAKeys():
+    # Random prime numbers
+    p = getRandomPrime()
+    q = getRandomPrime()
+    # Public Key
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = relativePrime(phi)
+    # Private Key
+    d = inverse(e,phi)
+    #print(d)
+    #print(relativePrime(phi))
+    return (n, e, d)
