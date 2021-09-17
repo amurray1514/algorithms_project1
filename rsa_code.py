68629d4fb699b5dbda8a55e77b0812803d0cbe94
Archer Murray <amurray@cub.uca.edu>
2021-09-17 11:23:17 -0500

    Adding integration test code to rsa_code

--- rsa_code.py
+++ rsa_code.py
@@ -77,3 +77,78 @@ def getRSAKeys():
     #print(d)
     #print(relativePrime(phi))
     return (n, e, d)
+
+"""
+Integration test for RSA code above.
+
+@author: Archer Murray
+"""
+def integrationTest():
+    # Generate RSA keys
+    print("Generating RSA keys, please wait...")
+    (n, e, d) = getRSAKeys()
+    print("RSA keys generated.")
+    enc_messages = []
+    choice = 0
+    # Display user interface
+    while not choice == 3:
+        print("Who are you?")
+        print("1. A public user")
+        print("2. The owner of the keys")
+        print("3. Exit program")
+        choice = int(input("Enter your choice: "))
+        # 1. A public user
+        while choice == 1:
+            print("As a public user, what would you like to do?")
+            print("1. Send an encrypted message")
+            print("2. Authenticate a digital signature")
+            print("3. Log out")
+            choice = int(input("Enter your choice: "))
+            # 1. Send a message
+            if choice == 1:
+                msg = input("Enter a message: ")
+                enc_messages.append(encrypt(msg, e, n))
+                print("Message encrypted and sent.")
+            # 2. Authenticate a signature
+            elif choice == 2:
+                print("Function not yet supported.")
+            # Return to loop if not logged out
+            if not choice == 3:
+                choice = 1
+            else:
+                choice = 0
+        # 2. The owner of the keys
+        while choice == 2:
+            print("As the owner of the keys, what would you like to do?")
+            print("1. Decrypt a received message")
+            print("2. Digitally sign a message")
+            print("3. Log out")
+            choice = int(input("Enter your choice: "))
+            # 1. Decrypt a message
+            if choice == 1:
+                if len(enc_messages) == 0:
+                    print("You have no messages available.")
+                else:
+                    print("The following messages are available:")
+                    for i in range(len(enc_messages)):
+                        print(i + 1, ". (length = ",
+                              len(enc_messages[i]), ")", sep = '')
+                    choice = int(input("Enter your choice: "))
+                    if choice < 1 or choice > len(enc_messages):
+                        print("Out of range, try again.")
+                    else:
+                        msg = decrypt(enc_messages[choice - 1], d, n)
+                        print("Decrypted message:", msg)
+                    choice = 0
+            # 2. Sign a message
+            elif choice == 2:
+                print("Function not yet supported.")
+            # Return to loop if not logged out
+            if not choice == 3:
+                choice = 2
+            else:
+                choice = 0
+
+# Run integration test
+if __name__ == '__main__':
+    integrationTest()
